3ce33ad7036848bd50c18e253f35c5fc1f49cb99
Tural Piriyev <piriyevtural00@gmail.com>
2025-07-23 02:31:31 +0400

    Initial Commit

--- StartUp01-55f72aec6242f69910f24cf2a6a533a5a31a6204/stylelint.config.js
+++
@@ -1,5 +0,0 @@
-// stylelint.config.js
-module.exports = {
-  plugins: ['stylelint-order', 'stylelint-config-tailwindcss'],
-  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
-};
